307201f7a00bce272396bc55ba0b9874d56fc8bf
Omer Koyuncu <omerkoyuncutr@gmail.com>
2024-05-31 17:35:11 +0300

    frontend->  booklist ve bookitem componentleri eklendi, booksview sayfası düzenlendi

--- frontend/src/views/BooksView.vue
+++ frontend/src/views/BooksView.vue
@@ -3,19 +3,25 @@
         <div class="container">
             <SectionHeader title="Books"
                 text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, quod!" />
+            <BookList :books="books" />
         </div>
     </section>
 </template>
 
 <script>
 import SectionHeader from "@/components/SectionHeader.vue"
-
+import BookList from "@/components/BookList.vue"
+import books from "@/db.js"
 export default {
     name: "BooksView",
     components: {
-        SectionHeader
+        SectionHeader,
+        BookList
+    },
+    data() {
+        return {
+            books: books
+        }
     }
 }
 </script>
-
-<style lang="scss" scoped></style>
