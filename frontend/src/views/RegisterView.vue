4044f9711ff75f3baa039a6208fed83a38ca1f66
Omer Koyuncu <omerkoyuncutr@gmail.com>
2024-06-03 14:19:08 +0300

    frontend-> registerview form validation duzeltme

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,38 +1,65 @@
 <template>
     <section class="full-section-height">
         <div class="container">
+            <div class="d-flex justify-content-center">
+                <h1 class="display-3">Register</h1>
+            </div>
             <form @submit.prevent="submitForm">
                 <!-- Username Field -->
                 <div class="row justify-content-center">
                     <div class="col-md-6 col-8 mb-3">
-                        <label for="username" class="form-label">Username</label>
+                        <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
                         <input type="text" class="form-control" id="username" name="username"
-                            v-model.trim="formData.username" required>
+                            v-model.trim="formData.username" required :class="{
+                                'is-valid': isUsernameValid,
+                                'is-invalid': !isUsernameValid && showUsernameWarningMessage,
+                            }" @focus="showUsernameWarningMessage = true" @blur="showUsernameWarningMessage = false">
+                        <span v-if="showUsernameWarningMessage && !isUsernameValid" class="text-danger small">Username
+                            must be between 5 and 20 characters!</span>
                     </div>
                 </div>
 
                 <!-- Email Field -->
                 <div class="row justify-content-center">
                     <div class="col-md-6 col-8 mb-3">
-                        <label for="email" class="form-label">Email</label>
+                        <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
                         <input type="email" class="form-control" id="email" name="email" v-model.trim="formData.email"
-                            required>
+                            required :class="{
+                                'is-valid': isEmailValid,
+                                'is-invalid':
+                                    (!isEmailValid && showEmailWarningMessage) ||
+                                    existingEmail === formData.email,
+                            }" @focus="showEmailWarningMessage = true" @blur="showEmailWarningMessage = false">
+
+                        <span v-if="showEmailWarningMessage && !isEmailValid" class="text-danger small">Please provide a
+                            valid email!
+                        </span>
+                        <span v-if="existingEmail === formData.email" class="text-danger small">
+                            {{ `${existingEmail} is already exist!` }}
+                        </span>
                     </div>
                 </div>
 
                 <!-- Password Field -->
                 <div class="row justify-content-center">
                     <div class="col-md-6 col-8 mb-3">
-                        <label for="password" class="form-label">Password</label>
+                        <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
                         <input type="password" class="form-control" id="password" name="password"
-                            v-model.trim="formData.password" required>
+                            v-model.trim="formData.password" required :class="{
+                                'is-valid': isPasswordValid,
+                                'is-invalid': !isPasswordValid && showPasswordWarningMessage,
+                            }" @focus="showPasswordWarningMessage = true" @blur="showPasswordWarningMessage = false">
+                        <span v-if="showPasswordWarningMessage && !isPasswordValid" class="text-danger small">Password
+                            must be between 4 and 10 characters!</span>
                     </div>
                 </div>
 
                 <!-- Submit Button -->
                 <div class="row justify-content-center">
                     <div class="col-md-6 col-8 mb-3">
-                        <button type="submit" class="btn btn-primary w-100">Register</button>
+                        <button :disabled="!isFormValid" type="submit" class="btn btn-primary w-100">Register</button>
+                        <span v-if="!isFormValid" class="text-danger small">* Please complete all of the required
+                            fields!</span>
                     </div>
                 </div>
             </form>
@@ -51,8 +78,33 @@ export default {
                 username: '',
                 email: '',
                 password: '',
-            }
+            },
+            showUsernameWarningMessage: false,
+            showEmailWarningMessage: false,
+            showPasswordWarningMessage: false,
+            existingEmail: null,
+            showGenericWarningMessage: false,
         }
+    },
+    computed: {
+        isFormValid() {
+            return this.isUsernameValid && this.isEmailValid && this.isPasswordValid;
+        },
+        isUsernameValid() {
+            return (
+                this.formData.username.length >= 5 &&
+                this.formData.username.length <= 20
+            );
+        },
+        isEmailValid() {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.formData.email);
+        },
+        isPasswordValid() {
+            return (
+                this.formData.password.length >= 4 &&
+                this.formData.password.length <= 10
+            );
+        },
     },
     methods: {
         ...mapActions(useAuthStore, ['register']),
@@ -60,13 +112,44 @@ export default {
             try {
                 await this.register(this.formData)
                 this.$router.push('/login')
-            } catch (error) {
-                console.log('Register failed!', error)
-
+            } catch (data) {
+                const { error } = data;
+                if (error === 'Email already exist!') {
+                    this.existingEmail = this.formData.email;
+                } else {
+                    this.showGenericWarningMessage = true;
+                    this.formData = {
+                        username: '',
+                        email: '',
+                        password: '',
+                    };
+                }
             }
         }
     }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.form-control {
+    border-radius: 25px;
+    height: 48px;
+}
+
+.form-control:focus {
+    box-shadow: none;
+}
+
+.btn-primary {
+    border-radius: 25px;
+    height: 48px;
+    background-color: var(--secondary-color);
+    border: 1px solid var(--secondary-color);
+}
+
+.btn-primary:hover {
+    background-color: #fff;
+    color: var(--secondary-color);
+    transition: all 0.3s ease;
+}
+</style>
