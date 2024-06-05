6caf0db228e715ed62ba3a40502610336fb36ee0
Omer Koyuncu <omerkoyuncutr@gmail.com>
2024-06-05 20:51:59 +0300

    frontend-> ratingstore duzeltme

--- frontend/src/stores/ratingStore.js
+++ frontend/src/stores/ratingStore.js
@@ -1,10 +1,11 @@
 import { defineStore } from "pinia";
 import axios from "axios";
+import { useBookStore } from "./bookStore.js";
 
 export const useRatingStore = defineStore("ratingStore", {
   state: () => ({
     ratings: [],
-    ratingsForBook: []
+    ratingsForBook: [],
   }),
 
   actions: {
@@ -22,12 +23,23 @@ export const useRatingStore = defineStore("ratingStore", {
     },
 
     async fetchRatingsForBook(bookId) {
+      const bookStore = useBookStore();
       try {
         const response = await axios.get(
           `http://localhost:3000/api/v1/ratings/book/${bookId}`
         );
 
         this.ratingsForBook = response.data.ratings;
+
+        const bookIndex = bookStore.books.findIndex(
+          (book) => book._id === bookId
+        );
+
+        if (bookIndex !== -1) {
+          bookStore.books[bookIndex].ratings = response.data.ratings;
+        } else {
+          console.warn(`Book with id: ${bookId} not found`);
+        }
       } catch (error) {
         console.error("Error fetchRatingsForBook!", error);
         throw error.response.data;
@@ -42,7 +54,9 @@ export const useRatingStore = defineStore("ratingStore", {
         );
         const updatedRatingData = response.data.rating;
 
-        const ratingIndex = this.ratings.findIndex((rating) => rating._id === ratingId);
+        const ratingIndex = this.ratings.findIndex(
+          (rating) => rating._id === ratingId
+        );
         if (ratingIndex !== -1) {
           this.ratings.splice(ratingIndex, 1, updatedRatingData);
         }
